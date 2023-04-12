533b3c1481319dd4ea3097eba7f04ae1c9cc6cee
yanwenzhenyanwenzhen <104296639+yanwenzhenyanwenzhen@users.noreply.github.com>
2023-04-12 15:14:15 +0800

    Add files via upload

---
+++ src/test/java/com/example/notepad/database/SQLiteHelperTest.java
@@ -0,0 +1,38 @@
+package com.example.notepad.database;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.ListView;
+
+import com.example.notepad.R;
+import com.example.notepad.adapter.NotepadAdapter;
+import com.example.notepad.bean.NotepadBean;
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SQLiteHelperTest extends TestCase {
+    private SQLiteDatabase sqLiteDatabase;
+
+    public void testOnCreate() {
+    }
+
+    public void testOnUpgrade() {
+    }
+
+    public void testInsertData() {
+    }
+
+    public void testDeleteData() {
+    }
+
+    public void testUpdateData() {
+    }
+
+    public void testQuery() {
+
+    }
+}
